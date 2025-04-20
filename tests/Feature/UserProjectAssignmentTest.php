0698027db64df4c93711e32404d77384b5da919e
MShJSTM <gmodern2560@gmail.com>
2025-04-20 18:41:57 +0330

    test assignment

--- tests/Feature/UserProjectAssignmentTest.php
+++ tests/Feature/UserProjectAssignmentTest.php
@@ -4,34 +4,16 @@ use App\Models\Project;
 use App\Models\User;
 
 use function Pest\Laravel\deleteJson;
+use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 
-it('can assign project to another user', function () {
+it('owner can assign his project to other users', function () {
     $user = User::factory()->create();
     $token = $user->createToken('auth-token')->plainTextToken;
 
     $project = Project::factory()->create();
 
-    $response = postJson('/api/projects/' . $project->id . '/assign', [
-        'user_id' => User::factory()->create()->id,
-        'role' => 'member',
-    ], [
-        'Authorization' => 'Bearer ' . $token,
-    ]);
-
-    $response->assertCreated()
-             ->assertJson([
-                 'message' => 'User assigned to project successfully.',
-             ]);
-});
-
-it('only owner can assign his project to other users', function () {
-    $user = User::factory()->create();
-    $token = $user->createToken('auth-token')->plainTextToken;
-
-    $project = Project::factory()->create();
-
-    $response = postJson('/api/projects/' . $project->id . '/assign', [
+    $response = postJson('/api/projects/'.$project->slug.'attach', [
         'user_id' => User::factory()->create()->id,
         'role' => 'member',
     ], [
@@ -60,5 +42,27 @@ it('can detach users from his own projects', function () {
     $response->assertOk()
              ->assertJson([
                  'message' => 'User detached from project successfully.',
+             ]);
+});
+
+it('assigned user can see his projects', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('auth-token')->plainTextToken;
+
+    $project = Project::factory()->create();
+    $user->projects()->attach($project, ['role' => 'member']);
+
+    $response = getJson('/api/projects', [
+        'Authorization' => 'Bearer ' . $token,
+    ]);
+
+    $response->assertOk()
+             ->assertJson([
+                 'data' => [
+                     [
+                         'id' => $project->id,
+                         'name' => $project->name,
+                     ],
+                 ],
              ]);
 });
